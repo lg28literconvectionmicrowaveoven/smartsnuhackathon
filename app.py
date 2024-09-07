c58cb75caefdfaf12b394e6d716ca325600dca27
Sabarigirish Manikandan <sabari.girish2057@gmail.com>
2024-09-07 08:31:43 +0530

    am i cooked?

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import csv
 import io
-import os
 
 from flask import Flask, jsonify, request
-from models import filter_data, load_data
+from models import load_data
 
 app = Flask(__name__)
 
 # Load the CSV data when the app starts
 data, df = load_data("data.csv")
+df = df.dropna(how="all", axis=0)
 
 
 @app.route("/")
@@ -20,35 +20,6 @@ def home():
 @app.route("/api/data/fetch_all", methods=["GET"])
 def get_all_data():
     return jsonify(data)
-
-
-# API route to filter data by parameters (temp, ph, conductivity, etc.)
-@app.route("/api/data/filter", methods=["GET"])
-def filter_by_params():
-    # Get query params
-    filters = {
-        "station_code": request.args.get("station-code"),
-        "location": request.args.get("location"),
-        "temperature_min": request.args.get("temperature-min"),
-        "temperature_max": request.args.get("temperature-max"),
-        "ph_min": request.args.get("ph-min"),
-        "ph_max": request.args.get("ph-max"),
-        "conductivity_min": request.args.get("conductivity-min"),
-        "conductivity_max": request.args.get("conductivity-max"),
-        "coliform_min": request.args.get("coliform-min"),
-        "coliform_max": request.args.get("coliform-max"),
-        "dissolved_solids_min": request.args.get("dissolved-solids-min"),
-        "dissolved_solids_max": request.args.get("dissolved-solids-max"),
-        "fluoride_min": request.args.get("fluoride-min"),
-        "fluoride_max": request.args.get("fluoride-max"),
-        "arsenic_min": request.args.get("arsenic-min"),
-        "arsenic_max": request.args.get("arsenic-max"),
-    }
-
-    # Filter data using the model's function
-    filtered_data = filter_data(df, filters)
-
-    return jsonify(filtered_data)
 
 
 @app.route("/api/data/write", methods=["POST"])
